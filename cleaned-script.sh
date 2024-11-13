3c9f7aa0f24b8122dc53088fdf67ae1c080228f8
Nguyen Ngoc Sy <synn.22it@vku.udn.vn>
2024-11-13 09:11:15 +0700

    Fix shellcheck issues and update workflow

--- cleaned-script.sh
+++ cleaned-script.sh
@@ -8,14 +8,8 @@ fi
 
 HOST=$1
 
-# Yêu cầu người dùng nhập thêm thông tin (ví dụ: một địa chỉ email)
-read -r -p "Enter your email: " email
-
-# In ra thông tin vừa nhập
-echo "You entered email: $email"
-
 # Ping kiểm tra kết nối
-if ping -c 4 "$HOST" > /dev/null; then
+if ping -c 4 $HOST > /dev/null;then
     echo "Network check: $HOST is reachable."
 else
     echo "Network check: $HOST is not reachable."
